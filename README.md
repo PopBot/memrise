af6e5eda31aa9b399bce813744db6c3ef88bfb04
Jarod Wong <jarodjaw@gmail.com>
2022-06-17 13:07:47 -0700

    Almost done.

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Memrise
 
+Requirements:
+- Install PortAudio
+
+> Note: If your PortAudio installation is through Homebrew, you will need to direct pip to the path to install a package requirement (pyaudio), so run
+> the following: ```pip install --global-option='build_ext' --global-option="-I$(brew --prefix)/include" --global-option="-L$(brew --prefix)/lib" pyaudio```
+> to properly install pyaudio.
+
 To run:
 ```shell
+$ pip install -r requirements.txt
 $ python3 memrise.py
 ```
 
--- main.py
+++ main.py
@@ -1,9 +1,76 @@
+import speech_recognition
 import speech_recognition as sr
 import sys
+from pydub import AudioSegment
+from pydub.playback import play
+import re
+import os
+import gtts
+from playsound import playsound
+from io import BytesIO
 
 
+def intake(mic, r):
 
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(r, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(mic, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    with mic as source:
+        r.adjust_for_ambient_noise(source, duration=3)
+        audio = r.listen(source)
+
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    # try recognizing the speech in the recording
+    # if a RequestError or UnknownValueError exception is caught,
+    #     update the response object accordingly
+    try:
+        response["transcription"] = r.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    print(response)
+    return response
 
 
 if __name__ == '__main__':
-    pass
+    mp3_fp = BytesIO()
+    print("Welcome to MEMRISE.")
+    print(speech_recognition.Microphone.list_microphone_names())
+    mic_index = 0
+    try:
+        mic_index = int(input("Enter the index of the microphone: "))
+    except ValueError:
+        mic_index = 0
+        print("Using default microphone at index 0.")
+
+    microphone = sr.Microphone(device_index=mic_index)
+    recognizer = sr.Recognizer()
+    phrase = input("Enter the phrase you want to memorize: ")
+    print("You entered: \n" + phrase)
+    tts = gtts.gTTS(text=phrase, lang="en")
+    filename = "input.mp3"
+    tts.write_to_fp(mp3_fp)
+    song = AudioSegment.from_file(mp3_fp, format="mp3")
+    play(song)
+    phrase = phrase.lower()
+    phrase = re.sub(r'[^\w\s]', '', phrase)
+    phrase_length = len(phrase)
+    print("Tokens: " + str(phrase_length))
+    tokens = phrase.split(" ")
+    print("Tokens: " + str(tokens))
+
+    # intake(microphone, recognizer)
