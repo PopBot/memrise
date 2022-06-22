742b1f7a2e9426e9fac75ae3b104390d886ac5fb
Jarod Wong <jarodjaw@gmail.com>
2022-06-21 17:44:45 -0700

    Pre mp3 render.

---
+++ convert_verses_to_mp3.py
@@ -0,0 +1,17 @@
+import csv
+import text_to_mp3
+
+
+def convert_verses_to_mp3(csv_file):
+    with open(csv_file, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            print(row[0])
+            full_verse = row[0] + " — " + row[1]
+            mp3_file = row[1].replace(" ", "_") + ".mp3"
+            text_to_mp3.text_to_mp3(full_verse, mp3_file)
+
+
+if __name__ == "__main__":
+    convert_verses_to_mp3("verses.csv")
+    print("Done!")
